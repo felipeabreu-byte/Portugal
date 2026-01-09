1dbf075d4190508e4e088fab20c329e74df98fba
macios <agtecnologialp@gmail.com>
2026-01-09 18:30:40 -0300

    fix: force dynamic api route to avoid build error v19

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -78,4 +78,16 @@ export const authOptions: NextAuthOptions = {
             return token;
         },
     },
+    secret: process.env.NEXTAUTH_SECRET,
+    cookies: {
+        sessionToken: {
+            name: `__Secure-next-auth.session-token`,
+            options: {
+                httpOnly: true,
+                sameSite: "lax",
+                path: "/",
+                secure: process.env.NODE_ENV === "production",
+            },
+        },
+    },
 };
